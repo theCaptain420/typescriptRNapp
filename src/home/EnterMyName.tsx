4cb0042e72ef11e9d45212a5ad33d7a56a5794fd
thecaptain420 <mads.nielsen@hotmail.com>
2020-06-24 20:39:57 +0200

    made up some stuff

---
+++ src/home/EnterMyName.tsx
@@ -0,0 +1,30 @@
+import React from "react"
+import { Button, StyleSheet, Text, View } from "react-native"
+
+//props, ? means not required
+export interface Props{
+    favoriteNumber:number
+    name?:string
+}
+
+interface State{
+    name:string
+    favoriteNumber:number
+}
+
+export default class EnterMyName extends React.Component<Props,State>{
+    //takes in props of type Props
+    constructor(props:Props){
+        super(props)
+
+        this.state={
+            //state is either the prop or nothing
+            name:props.name ||"",
+            //state here is given as must type
+            favoriteNumber:props.favoriteNumber
+        }
+    }
+
+    
+
+}
